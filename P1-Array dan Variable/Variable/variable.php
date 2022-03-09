8dd67f99330cf94397bf730d1a00eb446232ecd4
salmafathia15 <93049126+salmafathia15@users.noreply.github.com>
2022-03-09 22:18:01 +0700

    Add files via upload

---
+++ P1-Array dan Variable/Variable/variable.php
@@ -0,0 +1,18 @@
+<?php
+    // mendeklarasikan variabel
+    $nama = 'Aziz';
+    $umur = '20';
+    $berat = 57.0;
+
+    //menampilkan variabel
+    echo 'Nama : '.$nama.'<br>';
+    echo 'Umur : '.$umur.'<br>';
+    echo 'Berat : '.$berat.'<br>';
+
+    echo '<hr>';
+
+    //menampilkan variabel tanpa penghubung
+    echo "Nama : $nama<br>";
+    echo "Umur : $umur<br>";
+    echo "Berat : $berat<br>";
+?>
---
+++ P1-Array dan Variable/tugas_p1_SalmaFathia_0110121272_SI13/tugas1.php
@@ -0,0 +1,9 @@
+-Fungsi array_sort() digunakan untuk menyortir elemen-elemen dari array yang diindeks dalam ascending order (abjad untuk huruf dan angka untuk angka).
+-Fungsi array_pop() berguna untuk menghapus elemen terakhir dari sebuah array.
+-Fungsi array_push digunakan untuk menambahkan elemen baru ke akhir array.
+-Fungsi array_shift() menghapus elemen pertama dari array, dan mengembalikan nilai elemen yang dihapus.
+-Fungsi array_unshift() berguna untuk menyisipkan elemen baru ke array. Nilai array baru akan disisipkan di awal array.
+1. Apa perbedaan variabel user dengan variabel system?
+Perbedaan nya itu terletak. i admin system, Variable system bisa digunakan secara global oleh semua pengguna dan variable user itu hanya bisa digunakan oleh pengguna yang mendeklarasikan variable itu.
+2. Apa persamaan variabel system dengan variabel konstan?
+Variabel system adalah suatu tempat untuk menampung data yang nilainya selalu berubah. Sedangkan, Konstanta adalah suatu tempat untuk menampung data yang nilainya selalu tetap dan tidak pernah berubah. Jadi persamaan variabel system dan variabel konstan adalah sama. suna untuk menampung data.
